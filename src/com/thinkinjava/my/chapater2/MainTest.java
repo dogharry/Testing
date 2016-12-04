c64cf25c5bcbea663e920104f792d70653ac80e8
Harry Zhou (RD-CN) <harry_zhou@trendmicro.com.cn>
2016-12-04 19:53:10 +0800

    2

---
+++ src/com/thinkinjava/my/chapater2/MainTest.java
@@ -0,0 +1,13 @@
+package com.thinkinjava.my.chapater2;
+
+/**
+ * Created by harry_zhou on 2016-12-04.
+ */
+public class MainTest {
+    public static void main (String args[]){
+        for(String arg: args){
+            System.out.println("arg"+arg);
+        }
+
+    }
+}
